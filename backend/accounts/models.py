868de084a3a4362e912a5c9a4a5dee45af5ac3fc
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-07-09 00:46:54 +0430

    feat:project is now configured feat:custom user model feat:.env feat:staticroot and staticfiles

---
+++ backend/accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractUser,
+)
+from django.utils.translation import ugettext_lazy as _
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
